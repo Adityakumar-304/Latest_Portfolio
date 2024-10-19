3ef6105a8c9cf4db902ff1f736e36a0db69b3a84
Aditya kumar <128915236+Adityakumar-304@users.noreply.github.com>
2024-10-19 19:12:18 +0530

    Updated Portfolio

---
+++ Index.js
@@ -0,0 +1,33 @@
+//navbar action
+document.addEventListener("DOMContentLoaded", function () {
+  const menuToggle = document.querySelector('.menu-toggle');
+  const navLinks = document.querySelector('nav ul');
+
+  menuToggle.addEventListener('click', function () {
+      navLinks.classList.toggle('show');
+      menuToggle.classList.toggle('open');
+  });
+});
+
+
+//contact form
+function sendEmail(event) {
+  event.preventDefault(); // Prevent the form from submitting the default way
+
+  // Retrieve form field values
+  const name = document.getElementById('name').value;
+  const email = document.getElementById('email').value;
+  const phone = document.getElementById('phone').value;
+  const message = document.getElementById('message').value;
+
+  // Create the mailto link with the form data
+  const subject = encodeURIComponent('New Message from Portfolio Contact Form');
+  const body = encodeURIComponent(
+      `Name: ${name}\nEmail: ${email}\nPhone: ${phone}\n\nMessage:\n${message}`
+  );
+  const mailtoLink = `mailto:your-email@example.com?subject=${subject}&body=${body}`;
+
+  // Open the mailto link to trigger the email client
+  window.location.href = mailtoLink;
+}
+
